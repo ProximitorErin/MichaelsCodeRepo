d5d4fe30b4a178ad317edafe9e31392d19eec4a9
Erin Ferguson <erin@proximitor.com>
2017-02-07 20:12:20 -0600

    Going to try to get the environment variable working

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings 5 from Spring Boot!";
+        return "Greetings 5 from Spring Boot: " + System.getenv("DB_PASSWORD");
     }
 
 }
