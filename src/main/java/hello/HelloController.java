1986ae394421c328581f04020c780715655d267c
Erin Ferguson <erin@proximitor.com>
2017-02-07 21:02:05 -0600

    Demonstrate connecting to MySQL from service

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,8 @@
 package hello;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,33 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings 5 from Spring Boot: " + System.getenv("DB_PASSWORD");
+    	
+    	String dbPassword = System.getenv("DB_PASSWORD");
+    	String findings = "";
+    	
+    	try {
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            findings += ex;
+        }
+		
+		Connection conn = null;
+		
+		try {
+		    conn =
+		       DriverManager.getConnection("jdbc:mysql://michaelsinstance.cg5hjai80h9e.us-east-1.rds.amazonaws.com:3306/michaelsdb?" +
+		                                   "user=michael&password=" + dbPassword);
+
+		    
+		} catch (SQLException ex) {
+		    // handle any errors
+		    findings += ex;
+		}
+		
+		findings += "clean!";
+		
+        return "Greetings 6 from Spring Boot: " + findings;
     }
 
 }
