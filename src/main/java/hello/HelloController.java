81314569504430e935b658fc1a5e293b3ee9ec6f
Erin Ferguson <erin@proximitor.com>
2017-02-22 17:53:06 -0600

    Added the package I forgot

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
