e31364f8cb62373a0f2e4967015ed08b9b7f92bf
Erin Ferguson <erin@proximitor.com>
2017-03-12 10:39:08 -0500

    Inverting date comparisons

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -272,13 +272,13 @@ public class HelloController {
 			PreparedStatement ps = c.prepareStatement("SELECT a.sportName, a.name as statName "
 					+ "FROM michaelsdb.Stats a, michaelsdb.Sports b "
 					+ "WHERE a.sportName = b.Name "
-					+ "AND b.SeasonStartDate BETWEEN ? AND ? "
-					+ "AND b.SeasonEndDate BETWEEN ? AND ? "
+					+ "AND ? BETWEEN b.SeasonStartDate AND b.SeasonEndDate "
+					+ "AND ? BETWEEN b.SeasonStartDate AND b.SeasonEndDate"
 					);
 			ps.setDate(1, new java.sql.Date(startDate.getTime()));
 			ps.setDate(2, new java.sql.Date(endDate.getTime()));
-			ps.setDate(3, new java.sql.Date(startDate.getTime()));
-			ps.setDate(4, new java.sql.Date(endDate.getTime()));
+			//ps.setDate(3, new java.sql.Date(startDate.getTime()));
+			//ps.setDate(4, new java.sql.Date(endDate.getTime()));
 			
 			ResultSet rs = ps.executeQuery();
 
