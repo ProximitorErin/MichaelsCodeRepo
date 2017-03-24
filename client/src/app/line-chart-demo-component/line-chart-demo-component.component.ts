0dcb3f608167782ec2c12cf78ad45dffc696c7e7
Erin Ferguson <erin@proximitor.com>
2017-03-23 21:24:57 -0500

    Trying to make the line chart smaller

--- client/src/app/line-chart-demo-component/line-chart-demo-component.component.ts
+++ client/src/app/line-chart-demo-component/line-chart-demo-component.component.ts
@@ -12,11 +12,11 @@ export class LineChartDemoComponentComponent implements OnInit {
 
   // lineChart
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [65, 59, 80], label: 'Series A'},
+    {data: [28, 48, 40], label: 'Series B'},
+    {data: [18, 48, 77], label: 'Series C'}
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any> = ['Game 1', 'Game 2', 'Game 3'];
   public lineChartOptions:any = {
     responsive: true
   };
