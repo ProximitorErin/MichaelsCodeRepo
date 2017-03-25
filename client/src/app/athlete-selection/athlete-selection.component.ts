0dc6b6c1322f19c79fd4f71f39da03e6be0ef462
Erin Ferguson <erin@proximitor.com>
2017-03-25 13:22:19 -0500

    Amended error getting athletes

--- client/src/app/athlete-selection/athlete-selection.component.ts
+++ client/src/app/athlete-selection/athlete-selection.component.ts
@@ -19,18 +19,19 @@ export class AthleteSelectionComponent implements OnInit {
   }
 
   constructor(private _tournamentService : TournamentsService) {
-    this._tournamentService.getAthletes()
-      .subscribe(
-        data => this.formatResult(data),
-        error => this.errorMessage = <any>error
-      );
+    
    }
 
    athletes: IAthlete[];
   errorMessage: string;
 
   ngOnInit() {
-    this.addNewSlide();
+    this._tournamentService.getAthletes()
+      .subscribe(
+        data => this.formatResult(data),
+        error => this.errorMessage = <any>error
+      );
+    //this.addNewSlide();
   }
 
   //The time to show the next photo
@@ -40,12 +41,12 @@ export class AthleteSelectionComponent implements OnInit {
     //Photos
     private slides:Array<any> = [];
 
-  private addNewSlide() {
+  /* private addNewSlide() {
          this.slides.push(
             {image:'http://www.fightingillini.com/images/2016/12/16/Brochu_Danielle_2MJ0732.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Danielle Brochu'},
             {image:'http://www.fightingillini.com/images/2016/12/16/Gunther_Alyssa_2MJ0750.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Alyssa Gunther'},
             {image:'http://www.fightingillini.com/images/2016/12/16/Carrillo_Alexis_2MJ0718.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Alexis Carrillo'}
         );
-    }
+    } */
 
 }
