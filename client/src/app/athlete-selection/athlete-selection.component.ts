b25cef6490c995c9b2d85239fd5df45f5d7c4ee3
Erin Ferguson <erin@proximitor.com>
2017-03-25 13:39:11 -0500

    Trying to undo the damage

--- client/src/app/athlete-selection/athlete-selection.component.ts
+++ client/src/app/athlete-selection/athlete-selection.component.ts
@@ -41,12 +41,12 @@ export class AthleteSelectionComponent implements OnInit {
     //Photos
     private slides:Array<any> = [];
 
-  /* private addNewSlide() {
+  private addNewSlide() {
          this.slides.push(
             {image:'http://www.fightingillini.com/images/2016/12/16/Brochu_Danielle_2MJ0732.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Danielle Brochu'},
             {image:'http://www.fightingillini.com/images/2016/12/16/Gunther_Alyssa_2MJ0750.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Alyssa Gunther'},
             {image:'http://www.fightingillini.com/images/2016/12/16/Carrillo_Alexis_2MJ0718.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Alexis Carrillo'}
         );
-    } */
+    }
 
 }
