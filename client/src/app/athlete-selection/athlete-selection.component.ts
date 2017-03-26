16d283e09ecffd7b876f3315e01ea1fb4ab6fef0
Erin Ferguson <erin@proximitor.com>
2017-03-26 09:22:40 -0500

    Those were some massive pictures! Fixing...

--- client/src/app/athlete-selection/athlete-selection.component.ts
+++ client/src/app/athlete-selection/athlete-selection.component.ts
@@ -14,6 +14,7 @@ export class AthleteSelectionComponent implements OnInit {
       this.athletes = data;
       this.slides = [];
       for (let athlete of this.athletes) {
+        athlete.image += '?width=182&height=250&scale=both&bgcolor=e84a27';
         this.slides.push(athlete);
       }
   }
@@ -31,7 +32,6 @@ export class AthleteSelectionComponent implements OnInit {
         data => this.formatResult(data),
         error => this.errorMessage = <any>error
       );
-    //this.addNewSlide();
   }
 
   //The time to show the next photo
@@ -41,12 +41,12 @@ export class AthleteSelectionComponent implements OnInit {
     //Photos
     private slides:Array<any> = [];
 
-  private addNewSlide() {
+  /* private addNewSlide() {
          this.slides.push(
             {image:'http://www.fightingillini.com/images/2016/12/16/Brochu_Danielle_2MJ0732.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Danielle Brochu'},
             {image:'http://www.fightingillini.com/images/2016/12/16/Gunther_Alyssa_2MJ0750.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Alyssa Gunther'},
             {image:'http://www.fightingillini.com/images/2016/12/16/Carrillo_Alexis_2MJ0718.jpg?width=182&height=250&scale=both&bgcolor=e84a27',text:'Alexis Carrillo'}
         );
-    }
+    } */
 
 }
