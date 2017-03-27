04acbe9002b68b866ac19d593853e4fa97d1a234
Erin Ferguson <erin@proximitor.com>
2017-03-26 21:08:39 -0500

    Just set the defaults to 0 - and I'm done!

--- client/src/app/athlete-performance.service.ts
+++ client/src/app/athlete-performance.service.ts
@@ -37,14 +37,14 @@ export class AthletePerformanceService {
     {
       console.log("ccc");
       return [
-        {"data":[0,1,0,2,0,0,0,0,0,1,0,2],"label":"assists"},
-        {"data":[4,5,2,6,2,6,8,4,7,7,1,3],"label":"atBats"},
-        {"data":[2,2,1,1,1,3,3,2,2,0,1,1],"label":"hits"},
-        {"data":[0,0,0,0,0,3,2,0,2,2,0,1],"label":"leftonbase"},
-        {"data":[5,3,2,2,3,2,5,5,5,2,2,2],"label":"putouts"},
-        {"data":[1,2,1,1,0,2,3,0,1,0,2,1],"label":"rbis"},
-        {"data":[1,1,0,1,0,1,2,1,1,0,1,0],"label":"runs"},
-        {"data":[0,2,0,0,0,2,1,0,1,0,0,0],"label":"strikeouts"}
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"assists"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"atBats"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"hits"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"leftonbase"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"putouts"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"rbis"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"runs"},
+        {"data":[0,0,0,0,0,0,0,0,0,0,0,0],"label":"strikeouts"}
       ];
     }
     console.log("ddd");
